cb41cffcae4eed7d8c6db66854eea8544c3710ac
Gregmorr <gmor@uoregon.edu>
2025-11-05 14:34:27 -0800

    Test, initial commit
    Got variables for players, teams and stats.

---
+++ nfldataset.py
@@ -0,0 +1,12 @@
+#player_name = x
+#team = y
+#pos = z
+#projected_pass_yds = a
+#projected_rush_yds = b
+#projected_rec_yds = c
+#projected_rec = d
+#projected_td = t
+
+#Scoring
+#Json File
+#pfsefkse[fks[fs]]
